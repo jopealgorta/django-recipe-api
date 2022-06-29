faa7995b64546ec0da104886c63f15555fab8459
José Algorta <jpalgort@gmail.com>
2022-06-29 00:55:26 -0300

    Refactor urls to be REST

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -20,6 +22,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema')),
     path('api/users/', include('user.urls')),
-    path('api/recipe/', include('recipe.urls'))
+    path('api/recipes/', include('recipe.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
